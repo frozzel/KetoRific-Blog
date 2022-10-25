bbd0b77c004db7560926830060bc26775a94b2d5
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-10-25 12:36:08 -0400

    mods done, connnect

---
+++ models/Post.js
@@ -0,0 +1,7 @@
+const { Sequelize, Model, DataTypes}= require('sequelize');
+const sequelize = require('../config/config');
+
+class Post extends Model {}
+Post.init({title: {type: DataTypes.STRING, allowNull: false}}, {body:{type: DataTypes.STRING, allowNull: false}},{image:{type: DataTypes.STRING}},{sequelize});
+
+module.exports= Post;
